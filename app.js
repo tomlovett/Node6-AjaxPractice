2795151e9a52114b0372ee6bfd0ea87067a2948c
Tom Lovett <tomlovett6@gmail.com>
2016-02-17 15:42:37 -0700

    submission.js first draft

--- app.js
+++
@@ -1,8 +0,0 @@
-home, submit, view-submissions, winner
-
-angular: Submission model (name, url, title, desc)
-catch for more than 8
-
-voteFor()
-
-Server-side
---
+++ server.js
@@ -0,0 +1,25 @@
+// submission module
+	// Submission model (name, url, title, desc)
+		// this.votes
+	// contest (submission, submission)
+		// vote for
+		// loser
+
+
+// Server-side
+index.html
+	// basic stuff? simple landing page
+	// hooray for this contest!
+submit.html
+	// entry form
+	// splitter function: if length == 8; no more entries for now; in express routing function
+viewentries.html
+	// ng-repeat contests
+	// voting by POST
+winner.html
+	// hooray for this guy!!
+entries.json
+	// interactive functions written in app.js
+		// getLength()
+		// remove(submission): pull, parse, remove, return
+		// add : (pull, parse) add (, return)
