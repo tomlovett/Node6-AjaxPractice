29980990a10dfdad80086902e9576964a528b088
Tom Lovett <tomlovett6@gmail.com>
2016-02-18 15:36:48 -0700

    built app architecture

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
-// submission module
-	// Submission model (name, url, title, desc)
-		// this.votes
-	// contest (submission, submission)
-		// vote for
-		// loser
+var app = require('express')()
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+
+var submissions = [
+	{
+		name  : 'David',
+		url   : 'https://www.youtube.com/watch?v=2s4slliAtQU',
+		title : 'Surfin\' USA',
+		desc  : 'The Beach Boys\' song "Surfin\' USA"'
+	},
+	{
+		name  : 'Noah',
+		url   : 'https://www.youtube.com/watch?v=KnPL5OXSBNE',
+		title : 'I Get Around',
+		desc  : 'The Beach Boys\' hit "I Get Around"'
+	},
+	{
+		name  : 'Brian',
+		url   : 'https://www.youtube.com/watch?v=AOMyS78o5YI',
+		title : 'God Only Knows',
+		desc  : 'The Beach Boys\' hit song "God Only Knows"'
+	}
+]
+
+
 
 // Server-side
 index.html
-	// basic stuff? simple landing page
-	// hooray for this contest!
-submit.html
-	// entry form
-	// splitter function: if length == 8; no more entries for now; in express routing function
+
 viewentries.html
 	// write contests to page
 	// voting by POST
